fe3d5d1af73b62c2d760a8efc8fa58cf85fc3b6d
Sarthik <sarthikgarg01@gmail.com>
2019-01-08 23:04:22 +0530

    Read More at bottom

--- Bulletin/app/src/main/java/garg/sarthik/bulletin/SecondActivity.java
+++ Bulletin/app/src/main/java/garg/sarthik/bulletin/SecondActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
